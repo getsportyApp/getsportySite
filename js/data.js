05267366b3578463186ef0b35628aca5b8bba295
dev538623 <devendra@darkhorsesports.in>
2017-09-22 12:30:13 +0530

    bug fix

---
+++ js/data.js
@@ -0,0 +1,177 @@
+function getResources()
+{
+    //alert('amit');
+   $.ajax({
+    url:"http://liveapp.getsporty.in/getSportyLite/litecontroller.php?act=blog_api&token_id=dhs2016&token=0,1,3",crossDomain: true ,success: function(result)
+    {
+        $( "#img1" ).hide();
+        data = JSON.parse(result);
+        data = data.data;
+        var resources_temp ='';
+        var res_url = '';
+        var title   = ''; 
+        var summary = ''; 
+        var s = 0;
+        for (var i = 0; i < data.length; i++)
+        {  
+             res_url     = 'blog_detail.html?n='+data[i]['id']+'';//data[i]['url'];
+             title       = data[i]['title'].substring(0,32);
+             summary     = data[i]['summary'].substring(0,200);
+            if (i==0)
+            {   
+                resources_temp +='<div class="carousel-item active" ><div class="container-fluid pd0"><div class="row"><div class="col-md-4" style="margin-top:20px;" ><div class="card" style="height:500px;" ><img class="img-fluid" src="http://portal.getsporty.in/uploads/resources/'+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+data[i]['title']+'</h4><p class="card-text">'+summary+'...</p><div class="bottombutton"><a href="'+res_url+'" class="btn btn-primary" target="_blank">Read More..</a></div></div></div></div>';
+            }
+            else
+            {
+                        s = i + 1;
+            if(s%3==0)
+             {
+
+               resources_temp +='<div class="col-md-4" style="margin-top:20px;" ><div class="card" style="height:500px;"><img class="img-fluid" src="http://portal.getsporty.in/uploads/resources/'+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+data[i]['title']+'</h4><p class="card-text">'+summary+'...</p><div class="bottombutton"><a href="'+res_url+'" class="btn btn-primary" target="_blank">Read More..</a></div></div></div></div></div></div></div><div class="carousel-item"><div class="container-fluid pd0"><div class="row">';   
+        }
+
+        else
+
+        {
+
+           resources_temp +='<div class="col-md-4" style="margin-top:20px;"><div class="card" style="height:500px;" ><img class="img-fluid" src="http://portal.getsporty.in/uploads/resources/'+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+data[i]['title']+'</h4><p class="card-text">'+summary+'...</p><div class="bottombutton"><a href="'+res_url+'" class="btn btn-primary" target="_blank">Read More..</a></div></div></div></div>'; 
+
+
+
+     }
+
+}
+
+        } // End of for Loop
+
+        $("#id1").html(resources_temp);
+
+    }}); //End of ajax
+
+
+
+} // End of Function
+
+
+
+
+    
+function getEvent()
+{
+ $.ajax(
+   {url: "http://localhost/testingapp/getSportyLite/litecontroller.php?act=event_and_tour_api",crossDomain: true ,success: function(result)
+       {
+       data = JSON.parse(result);
+        data = data.data;
+        var event_temp ='';
+        for (var i = 1; i <data.length; i++)
+        {
+            var name         = data[i]['name']; 
+            var Path         = data[i]['Path']; 
+            if(Path=='E') 
+            {
+
+            var image_path = "http://getsporty.in/portal/uploads/event/"
+            }
+            else
+            {
+                var image_path = "http://getsporty.in/portal/uploads/tournament/"
+            }
+            name             = name.substring(0,32);
+            var description  = data[i]['description']; 
+            description = description.substring(0,100);
+        if(data[i]['image']!='')
+        {
+            if (i==1)
+            {
+                event_temp +='<div class="carousel-item active" ><div class="col-md-4" ><div class="card" style="height:500px;" ><img class="img-fluid" src="'+image_path+''+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+name+'</h4><p class="card-text">'+description+'</p><a href="#" class="btn btn-primary">Read More..</a></div></div></div>';
+            }
+            else
+            {
+            if(i%3==0)
+            {
+                event_temp +='<div class="col-md-4" ><div class="card" style="height:500px;"><img class="img-fluid" src="'+image_path+''+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+name+'</h4><p class="card-text">'+description+'</p><a href="#" class="btn btn-primary">Read More..</a></div></div></div></div><div class="carousel-item">';   
+
+        }
+        else
+        {
+            event_temp +='<div class="col-md-4" ><div class="card" style="height:500px;" ><img class="img-fluid" src="'+image_path+''+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+name+'</h4><p class="card-text">'+description+'</p><a href="#" class="btn btn-primary">Read More..</a></div></div></div>'; 
+        }
+     }
+}
+        } // End of for Loop
+
+        $("#id2").html(event_temp);
+        console.log(event_temp);
+     
+
+    }}); //End of ajax
+
+
+
+} // End of Function
+
+
+
+
+function getJob()
+{
+   $.ajax({
+    url:"http://localhost/testingapp/getSportyLite/litecontroller.php?act=job_api",crossDomain: true ,success: function(result)
+    {
+        
+        data = JSON.parse(result);
+        data = data.data;
+        var job_temp ='';
+        var res_url = '';
+        var title   = ''; 
+        var summary = ''; 
+        var s = 0;
+        for (var i = 0; i < data.length; i++)
+        {  
+             title       = data[i]['title'].substring(0,13);
+             summary     = data[i]['description'].substring(0,60);
+            if (i==0)
+            {   
+                job_temp +='<div class="carousel-item active" ><div class="container-fluid pd0"><div class="row"><div class="col-md-4" style="margin-top:20px;" ><div class="card" style="height:500px;" ><img class="img-fluid" src="http://getsporty.in/portal/uploads/job/'+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+data[i]['title']+'</h4><p class="card-text">'+summary+'...</p><div class="bottombutton"><a href="'+res_url+'" class="btn btn-primary" target="_blank">Read More..</a></div></div></div></div>';
+            }
+            else
+            {
+                        s = i + 1;
+            if(s%3==0)
+             {
+
+               job_temp +='<div class="col-md-4" style="margin-top:20px;" ><div class="card" style="height:500px;"><img class="img-fluid" src="http://getsporty.in/portal/uploads/job/'+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+data[i]['title']+'</h4><p class="card-text">'+summary+'...</p><div class="bottombutton"><a href="'+res_url+'" class="btn btn-primary" target="_blank">Read More..</a></div></div></div></div></div></div></div><div class="carousel-item"><div class="container-fluid pd0"><div class="row">';   
+        }
+
+        else
+
+        {
+
+           job_temp +='<div class="col-md-4" style="margin-top:20px;"><div class="card" style="height:500px;" ><img class="img-fluid" src="http://getsporty.in/portal/uploads/job/'+data[i]['image']+'" alt="img"><div class="card-block"><h4 class="card-title">'+data[i]['title']+'</h4><p class="card-text">'+summary+'...</p><div class="bottombutton"><a href="'+res_url+'" class="btn btn-primary" target="_blank">Read More..</a></div></div></div></div>'; 
+
+
+
+     }
+
+}
+
+        } // End of for Loop
+
+        $("#id3").html(job_temp);
+
+    }}); //End of ajax
+
+
+
+} // End of Function
+
+
+
+
+
+
+
+
+
+
